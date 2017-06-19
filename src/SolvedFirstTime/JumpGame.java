bbee455d0be1f902520003dfb9d5e7425a88c50c
Duo Tao <taod@carleton.edu>
2017-06-19 15:06:23 -0500

    one more problem

--- src/SolvedFirstTime/JumpGame.java
+++ src/SolvedFirstTime/JumpGame.java
@@ -3,17 +3,33 @@ package SolvedFirstTime;
 import java.util.Arrays;
 
 /**
- * Leetcode Problem
+ * Leetcode Problems
  * https://leetcode.com/problems/jump-game-ii/
+ * https://leetcode.com/problems/jump-game/
  * At first I got a DP solution, but got TLE. Latter I thought really hard about the
  * greedy side of the problem and solved it.
  */
 public class JumpGame {
   public static void main(String...args) {
-    System.out.println(solve(new int[]{3, 1, 1 ,1}));
+    System.out.println(solve(new int[]{3,2,1,0,4}));
   }
 
-  public static int solve(int[] nums) {
+  public static boolean solve(int[] nums) {
+    boolean[] dp = new boolean[nums.length];
+    dp[0] = true;
+    for (int i = 1; i < nums.length; i++) {
+      for (int j = i - 1; j >= 0; j--) {
+        if (dp[j] && nums[j] >= i - j) {
+          dp[i] = true;
+          break;
+        }
+      }
+    }
+    return dp[nums.length - 1];
+  }
+
+
+  public static int solve2(int[] nums) {
     int[] minSource = new int[nums.length];
     Arrays.fill(minSource, -1);
     for (int i = nums.length - 1; i >= 0; i--) {
